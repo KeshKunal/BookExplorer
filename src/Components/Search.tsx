7dd377b35aa86899aa209c2017bb38cf382697a3
KeshKunal <infini2ude@gmail.com>
2025-10-12 19:18:16 +0530

    Implement search functionality

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/Search.css"
 import { FaSearch } from 'react-icons/fa'
+import type { AppDispatch } from '../redux/store';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { fetchSearchResults } from '../redux/BooksSlice';
 
 const Search: React.FC = () => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate(); 
+  const dispatch: AppDispatch = useDispatch();
+  const handleSubmit = (e: React.FormEvent) =>
+  {
+    e.preventDefault();
+    if(query.trim())
+    {
+        dispatch(fetchSearchResults(query))
+        navigate('/search');
+        setQuery("");
+    }
+  };
+
   return (
     <div className='search-section'>
         <div className='search-container'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='form-container'>
-                    <input type="text" placeholder='Search by Title, Author, Keword...'/>
+                    <input type="text" placeholder='Search by Title, Author, Keword...'
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}/>
                     <button type='submit' className='search-button'>
                         <FaSearch size={18} />
                     </button>
